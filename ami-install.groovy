f2823ff907892453f64d60ed550d0bdd4bef9f66
madanmk07 <30584127+madanmk07@users.noreply.github.com>
2017-10-09 22:22:38 +0530

    bug-pheww

--- ami-install.groovy
+++ ami-install.groovy
@@ -2,7 +2,7 @@ node('master') {
     git 'https://github.com/wardviaene/terraform-course'
     
     sh '''cd jenkins-packer-demo
-aws s3 cp s3://terraform-state-madanmk07/state/terraform.tfstate terraform.tfstate
+aws s3 cp s3://terraform-state-madanmk07/terraform/terraform.tfstate terraform.tfstate
 aws s3 cp s3://terraform-state-madanmk07/amivar.tf amivar.tf
 touch mykey
 touch mykey.pub
